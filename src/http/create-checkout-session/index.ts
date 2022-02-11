6b5dfc2ec9ff877eedc0e1d303a739741eef787d
cjavdev <cjavilla@gmail.com>
2022-02-11 10:36:45 -0500

    Try things

---
+++ src/http/create-checkout-session/index.ts
@@ -0,0 +1,39 @@
+// import Stripe from "https://esm.sh/stripe?target=deno";
+//
+// const stripe = Stripe(Deno.env.get("STRIPE_API_KEY"), {
+//   // This is needed to use the Fetch API rather than relying on the Node http
+//   // package.
+//   httpClient: Stripe.createFetchHttpClient(),
+// });
+
+
+export async function handler (req: object) {
+  // const session = await stripe.checkout.sessions.create({
+  //   payment_method_types: ['card'],
+  //   line_items: [
+  //     {
+  //       price_data: {
+  //         currency: 'usd',
+  //         product_data: {
+  //           name: 'T-shirt',
+  //         },
+  //         unit_amount: 2000,
+  //       },
+  //       quantity: 1,
+  //     },
+  //   ],
+  //   mode: 'payment',
+  //   success_url: 'https://example.com/success',
+  //   cancel_url: 'https://example.com/cancel',
+  // });
+
+  return {
+    statusCode: 200,
+    headers: {
+      'content-type': 'text/html; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    },
+    'test'
+    // JSON.stringify(session)
+  }
+}
