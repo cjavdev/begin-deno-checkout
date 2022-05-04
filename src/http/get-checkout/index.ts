5694a6f29696850222212f14c492bee17e0beba9
cjavdev <cjavilla@gmail.com>
2022-05-04 16:20:20 -0400

    testing

--- src/http/get-checkout/index.ts
+++ src/http/get-checkout/index.ts
@@ -9,32 +9,33 @@ const stripe = Stripe(Deno.env.get("STRIPE_API_KEY"), {
 
 export async function handler (req: object) {
   // /v1/checkout/sessions
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card'],
-    line_items: [
-      {
-        price_data: {
-          currency: 'usd',
-          product_data: {
-            name: 'T-shirt',
-          },
-          unit_amount: 2000,
-        },
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    success_url: 'https://example.com/success',
-    cancel_url: 'https://example.com/cancel',
-  });
+  // const session = await stripe.checkout.sessions.create({
+  //   payment_method_types: ['card'],
+  //   line_items: [
+  //     {
+  //       price_data: {
+  //         currency: 'usd',
+  //         product_data: {
+  //           name: 'T-shirt',
+  //         },
+  //         unit_amount: 2000,
+  //       },
+  //       quantity: 1,
+  //     },
+  //   ],
+  //   mode: 'payment',
+  //   success_url: 'https://example.com/success',
+  //   cancel_url: 'https://example.com/cancel',
+  // });
+  console.log("testing")
 
   return {
     statusCode: 303,
     headers: {
-      'location': session.url,
+      // 'location': session.url,
       'content-type': 'text/html; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
-    // body: 'test'
+    body: 'test'
   }
 }
